32f9d756b8eaa0dac3eaef95068cc658414f2b08
Nicklas Utgaard <nutgaard@gmail.com>
2022-08-01 08:55:41 +0200

    [KAIZEN-0] fjerne bruk av channel for rapportering av seltest-status
    hovedbolken av arbeidet som gjøres med å sjekke ett endepunkt gjøres før rapportering (evt vha ping-funksjonen), og det er derfor ikke behov for asynkronitet i selve rapporteringen.

--- kotlin-utils/src/main/kotlin/no/nav/personoversikt/utils/SelftestGenerator.kt
+++ kotlin-utils/src/main/kotlin/no/nav/personoversikt/utils/SelftestGenerator.kt
@@ -1,38 +1,15 @@
 package no.nav.personoversikt.utils
 
 import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.receiveAsFlow
 
-@OptIn(DelicateCoroutinesApi::class)
-class SelftestGenerator(private val config: Config) {
+class SelftestGenerator private constructor(private var config: Config) {
     companion object {
-        private val statusmap = mutableMapOf<String, Event>()
-        private val channel = Channel<Event>(capacity = Channel.BUFFERED)
-        private var aggregator = GlobalScope.launch {
-            channel.receiveAsFlow().collect {
-                statusmap[it.reporter.name] = it
-            }
-        }
-
-        internal fun register(event: InitEvent) {
-            statusmap[event.reporter.name] = event
-        }
-
-        internal fun restart() {
-            statusmap.clear()
-            aggregator = GlobalScope.launch {
-                channel.receiveAsFlow().collect {
-                    statusmap[it.reporter.name] = it
-                }
-            }
-        }
-
-        fun stop() {
-            aggregator.cancel()
+        private var instance = SelftestGenerator(Config())
+        fun getInstance(config: Config): SelftestGenerator {
+            instance.config = config
+            return instance
         }
     }
-
     open class Config(
         var appname: String = "Not set",
         var version: String = "Not set",
@@ -42,6 +19,16 @@ class SelftestGenerator(private val config: Config) {
     class InitEvent(reporter: Reporter) : Event(reporter)
     class OkEvent(reporter: Reporter) : Event(reporter)
     class ErrorEvent(reporter: Reporter, val error: Throwable) : Event(reporter)
+
+    private val statusmap = mutableMapOf<String, Event>()
+
+    internal fun register(event: Event) {
+        statusmap[event.reporter.name] = event
+    }
+
+    internal fun clear() {
+        statusmap.clear()
+    }
 
     fun isAlive(): Boolean {
         return statusmap.values.none { it is ErrorEvent && it.reporter.critical }
@@ -70,20 +57,22 @@ class SelftestGenerator(private val config: Config) {
 
     class Reporter(val name: String, val critical: Boolean) {
         init {
-            register(InitEvent(this))
+            instance.register(InitEvent(this))
         }
 
-        suspend fun reportOk() {
-            channel.send(OkEvent(this))
+        fun reportOk() {
+            instance.register(OkEvent(this))
         }
 
-        suspend fun reportError(error: Throwable) {
-            channel.send(ErrorEvent(this, error))
+        fun reportError(error: Throwable) {
+            instance.register(ErrorEvent(this, error))
         }
 
-        suspend fun ping(fn: suspend () -> Unit) {
+        fun ping(fn: suspend () -> Unit) {
             try {
-                fn()
+                runBlocking(Dispatchers.IO) {
+                    fn()
+                }
                 reportOk()
             } catch (e: Throwable) {
                 reportError(e)
