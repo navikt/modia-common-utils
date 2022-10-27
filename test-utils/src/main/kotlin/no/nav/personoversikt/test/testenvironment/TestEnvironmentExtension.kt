ff51a3e0f55b5b06596bb5255f86c942dac2981a
Nicklas Utgaard <nutgaard@gmail.com>
2022-10-27 14:28:06 +0200

    [KAIZEN-0] legge til lazy oppsett for testenvironment
    kan brukes i tilfeller hvor testenvironment ikke nødvendigvis er klart før man test-funksjonen kjører.
    E.g om man trenger å hente ut en verdi fra en annen extension som ikke er klar ved initialisering av extensions.

--- test-utils/src/main/kotlin/no/nav/personoversikt/test/testenvironment/TestEnvironmentExtension.kt
+++ test-utils/src/main/kotlin/no/nav/personoversikt/test/testenvironment/TestEnvironmentExtension.kt
@@ -3,7 +3,7 @@ package no.nav.personoversikt.test.testenvironment
 import org.junit.jupiter.api.extension.*
 
 class TestEnvironmentExtension(
-    testEnvironment: Map<String, String?> = emptyMap(),
+    testEnvironment: () -> Map<String, String?>
 ) : BeforeTestExecutionCallback,
     AfterTestExecutionCallback,
     ParameterResolver {
@@ -11,6 +11,7 @@ class TestEnvironmentExtension(
     private val testEnvironment: TestEnvironment = TestEnvironment(testEnvironment)
 
     constructor(vararg pairs: Pair<String, String?>) : this(mapOf(*pairs))
+    constructor(testEnvironment: Map<String, String?>) : this({ testEnvironment })
 
     override fun beforeTestExecution(context: ExtensionContext) {
         testEnvironment.beforeTestExecution()
