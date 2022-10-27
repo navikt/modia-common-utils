22c45a760ad5d085e2fba1e8d60e4b8e6e2754a6
Nicklas Utgaard <nutgaard@gmail.com>
2022-10-27 14:28:06 +0200

    [KAIZEN-0] legge til lazy oppsett for testenvironment
    kan brukes i tilfeller hvor testenvironment ikke nødvendigvis er klart før man test-funksjonen kjører.
    E.g om man trenger å hente ut en verdi fra en annen extension som ikke er klar ved initialisering av extensions.

--- test-utils/src/test/kotlin/no/nav/personoversikt/test/testenvironment/TestEnvironmentRuleTest.kt
+++ test-utils/src/test/kotlin/no/nav/personoversikt/test/testenvironment/TestEnvironmentRuleTest.kt
@@ -13,9 +13,18 @@ class TestEnvironmentRuleTest {
         "key1" to "value1"
     )
 
+    @Rule
+    @JvmField
+    val lazyenvironment = TestEnvironmentRule {
+        mapOf(
+            "key2" to "value2"
+        )
+    }
+
     @Test
     internal fun `should setup environment`() {
         assertEquals("value1", System.getProperty("key1"))
+        assertEquals("value2", System.getProperty("key2"))
     }
 
     companion object {
@@ -23,12 +32,14 @@ class TestEnvironmentRuleTest {
         @BeforeClass
         internal fun `key should not exist before test execution`() {
             assertEquals(null, System.getProperty("key1"))
+            assertEquals(null, System.getProperty("key2"))
         }
 
         @JvmStatic
         @AfterClass
         internal fun `key should not exist after test execution`() {
             assertEquals(null, System.getProperty("key1"))
+            assertEquals(null, System.getProperty("key2"))
         }
     }
 }
