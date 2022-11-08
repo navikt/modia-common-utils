59b4602c780e225b864db942ac72c00e74049185
Nicklas Utgaard <nutgaard@gmail.com>
2022-11-08 08:54:58 +0100

    [KAIZEN-0] tag commit ved release
    tags plukkes automatisk opp av jitpack, og vil lage en ny release hos dem

--- .github/release-beta.sh
+++ .github/release-beta.sh
@@ -4,11 +4,14 @@ set -e
 TIME=$(TZ="Europe/Oslo" date +%Y.%m.%d-%H.%M)
 COMMIT=$(git rev-parse --short=12 HEAD)
 VERSION="1.$TIME-$COMMIT-beta"
-echo "Setting version $VERSION"
+echo "Tagging commit: $VERSION"
 
+git tag $VERSION
+git push --tags
+
+echo "Setting version: $VERSION"
 mvn -B versions:set -DnewVersion="$VERSION"
 mvn -B versions:commit
 
 echo "Running release beta"
-#mvn -B --settings .github/maven-settings.xml deploy -Dmaven.wagon.http.pool=false
-git push --tags
+mvn -B --settings .github/maven-settings.xml deploy -Dmaven.wagon.http.pool=false
