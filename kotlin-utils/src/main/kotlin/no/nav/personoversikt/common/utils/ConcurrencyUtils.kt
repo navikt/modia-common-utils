88188ba019422ddaf32e146937f0f2ea61df7f91
Nicklas Utgaard <nutgaard@gmail.com>
2022-12-09 11:14:14 +0100

    [KAIZEN-0] legge til utils for concurrency

---
+++ kotlin-utils/src/main/kotlin/no/nav/personoversikt/common/utils/ConcurrencyUtils.kt
@@ -0,0 +1,41 @@
+package no.nav.personoversikt.common.utils
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
+import java.util.concurrent.CompletableFuture
+
+object ConcurrencyUtils {
+    fun <FIRST, SECOND> inParallel(first: () -> FIRST, second: () -> SECOND): Pair<FIRST, SECOND> {
+        val firstTask = CompletableFuture.supplyAsync(first)
+        val secondTask = CompletableFuture.supplyAsync(second)
+
+        CompletableFuture.allOf(firstTask, secondTask).get()
+
+        return Pair(firstTask.get(), secondTask.get())
+    }
+
+    fun <T> List<() -> T>.runInParallel(): List<T> {
+        return runBlocking(Dispatchers.IO) {
+            this@runInParallel.map {
+                async {
+                    it()
+                }
+            }.awaitAll()
+        }
+    }
+}
+
+fun <TYPE, RETURN> ThreadLocal<TYPE>.withValue(value: TYPE, block: () -> RETURN): RETURN {
+    val original = this.get()
+    this.set(value)
+    val result = block()
+    this.set(original)
+    return result
+}
+
+fun <TYPE, RETURN> ThreadLocal<TYPE>.makeTransferable(block: () -> RETURN): () -> RETURN {
+    val current = this.get()
+    return { this.withValue(current, block) }
+}
--- kotlin-utils/src/main/kotlin/no/nav/personoversikt/common/utils/KotlinUtils.kt
+++ kotlin-utils/src/main/kotlin/no/nav/personoversikt/common/utils/KotlinUtils.kt
@@ -4,6 +4,8 @@ object KotlinUtils {
     infix fun Int.inRange(range: Pair<Int, Int>): Boolean = this >= range.first && this < range.second
     infix fun Int.inRangeInclusive(range: Pair<Int, Int>): Boolean = this >= range.first && this <= range.second
 
+    infix fun Int.plusminus(offset: Int): IntRange = (this - offset)..(this + offset)
+
     infix fun <T> ((T) -> Boolean).or(other: (T) -> Boolean): (T) -> Boolean = { this(it) or other(it) }
     infix fun <T> ((T) -> Boolean).and(other: (T) -> Boolean): (T) -> Boolean = { this(it) and other(it) }
     infix fun <T> ((T) -> Boolean).xor(other: (T) -> Boolean): (T) -> Boolean = { this(it) xor other(it) }
